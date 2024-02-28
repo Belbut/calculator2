edc2e00e3b1fa156e2a8761bea6b8039e8fb2feb
Belbut <80793911+Belbut@users.noreply.github.com>
2024-02-28 20:58:44 +0000

    Create calculator basic logic

---
+++ script.js
@@ -0,0 +1,44 @@
+/* let add = (a, b) => a + b,
+    subtract = (a, b) => a - b,
+    multiply = (a, b) => a * b,
+    divide = (a, b) => a / b;
+
+let methods = {
+    "+": (a,b)=>add(a,b),
+    "-": (a,b)=>subtract(a,b),
+    "x": (a,b)=>multiply(a,b),
+    "/": (a,b)=>divide(a,b)
+
+}
+
+function calculate(a, operator, b) {
+
+} */
+
+function Calculator() {
+    this.variables = [];
+
+    const methods = {
+        "+": (a, b) => a + b,
+        "-": (a, b) => a - b,
+        "x": (a, b) => a * b,
+        "/": (a, b) => {
+            if (b == 0) return Infinity
+            return a / b
+        }
+    }
+
+    this.operate = function (calculationArr) {
+        let number1 = +calculationArr[0];
+        let operator = calculationArr[1];
+        let number2 = +calculationArr[2];
+
+        let result = this.methods[operator](number1, number2)
+
+        let newCalculationArr = calculationArr.toSpliced(0, 3, result)
+
+        if (newCalculationArr.length > 1) return this.operate(newCalculationArr);
+
+        return result;
+    }
+}
