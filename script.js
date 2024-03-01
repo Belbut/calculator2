3ef6979e2c162f1453a589b6ae1d300a4099096f
Belbut <80793911+Belbut@users.noreply.github.com>
2024-03-01 16:39:38 +0000

    Add logic for number input

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 function Calculator(firstVariable = 0) {
     this.variables = [firstVariable];
 
+    let result;
+
+    this.addNumberInput = function (input) {
+        let lastVariable = this.variables[this.variables.length - 1];
+
+        if (isNaN(lastVariable)) {//last variable is a operator
+            this.variables.push(input)
+        } else {
+            this.variables[this.variables.length - 1] = lastVariable + input
+        }
+    }
+
+    this.addOperatorInput = function (input) {
+        let lastVariable = this.variables[this.variables.length - 1];
+
+        if (isNaN(lastVariable)) {//last variable is a operator
+            this.variables.pop();
+        }
+
+        this.variables.push(input)
+
+    }
+
+
+
+
     const methods = {
         "+": (a, b) => a + b,
         "-": (a, b) => a - b,
@@ -24,4 +50,42 @@ function Calculator(firstVariable = 0) {
 
         return result;
     }
+}
+
+let calculator = new Calculator(0)
+const buttons = document.querySelectorAll("button")
+buttons.forEach((btn) => btn.addEventListener("click", (btnEvent) => numberInjection(btnEvent.target.textContent || btnEvent.target.innerText, calculator)))
+
+let numberInjection = function (str, CalculatorObj) {
+    switch (str) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "0":
+            CalculatorObj.addNumberInput(str)
+
+            break;
+
+        case "C":
+            calculator = new Calculator();
+            break;
+
+        case "+":
+        case "-":
+        case "/":
+        case "x":
+            CalculatorObj.addOperatorInput(str)
+            break;
+            
+        default:
+            break;
+    }
+    console.log(CalculatorObj.variables)
+
 }
